fc4f82ae13d3c16c704a7e5015b262f5cf1fe8a4
Connor Schmidt <connorschmidt@Connors-iMac.local>
2023-12-28 16:38:37 -0500

    improve shield tester output

--- Custom_VideoShield_Tester/Custom_VideoShield_Tester.ino
+++ Custom_VideoShield_Tester/Custom_VideoShield_Tester.ino
@@ -11,11 +11,17 @@
 #define button3Pin 11
 #define button4Pin 10
 
-//Initialize button value variables:
-int button1 = 0;
-int button2 = 0;
-int button3 = 0;
-int button4 = 0;
+// Initialize button press count variables:
+byte button1 = 0;
+byte button2 = 0;
+byte button3 = 0;
+byte button4 = 0;
+
+// Initialize last pot value variables:
+int potRead1_last = 0;
+int potRead2_last = 0;
+int potRead3_last = 0;
+int potRead4_last = 0;
 
 void setup() {
   //Start serial
@@ -26,6 +32,12 @@ void setup() {
   pinMode(button2Pin, INPUT_PULLUP);
   pinMode(button3Pin, INPUT_PULLUP);
   pinMode(button4Pin, INPUT_PULLUP);
+
+  // Get initial values of pots
+  potRead1_last = analogRead(pot1Pin);
+  potRead2_last = analogRead(pot2Pin);
+  potRead3_last = analogRead(pot3Pin);
+  potRead4_last = analogRead(pot4Pin);
 }
 
 void loop() {
@@ -36,18 +48,11 @@ void loop() {
   int potRead3 = analogRead(pot3Pin);
   int potRead4 = analogRead(pot4Pin);
 
-  //Display pot values:
-  Serial.print("Pot1 = "); 
-  Serial.println(potRead1); 
-  Serial.print("Pot2 = "); 
-  Serial.println(potRead2); 
-  Serial.print("Pot3 = "); 
-  Serial.println(potRead3); 
-  Serial.print("Pot4 = "); 
-  Serial.println(potRead4); 
-  Serial.println(); //add some spacing
-  Serial.println();
-  
+  // Display pot values if changed
+  potRead1_last = print_pot_values(1, potRead1_last, potRead1);
+  potRead2_last = print_pot_values(2, potRead2_last, potRead2);
+  potRead3_last = print_pot_values(3, potRead3_last, potRead3);
+  potRead4_last = print_pot_values(4, potRead4_last, potRead4);
   
   //Read button values:
   //NOTE: Button press == LOW
@@ -56,39 +61,39 @@ void loop() {
   int buttonRead3 = digitalRead(button3Pin);
   int buttonRead4 = digitalRead(button4Pin);
 
-  //if button pressed, increase value of button variable
-  if (buttonRead1 == LOW) {
-     button1 = button1 + 1;
-     delay(20);
-  }
-
-  if (buttonRead2 == LOW) {
-     button2 = button2 + 1;
-     delay(20);
-  }
-
-  if (buttonRead3 == LOW) {
-     button3 = button3 + 1;
-     delay(20);
-  }
-
-  if (buttonRead4 == LOW) {
-     button4 = button4 + 1;
-     delay(20);
-  }
-
-  //Display button values:
-  Serial.print("Button1 = "); 
-  Serial.println(button1); 
-  Serial.print("Button2 = "); 
-  Serial.println(button2); 
-  Serial.print("Button3 = "); 
-  Serial.println(button3); 
-  Serial.print("Button4 = "); 
-  Serial.println(button4); 
-  Serial.println(); //add some spacing
-  Serial.println();
+  // Display button counter values if button pressed
+  button1 = print_button_values(1, buttonRead1, button1);
+  button2 = print_button_values(2, buttonRead2, button2);
+  button3 = print_button_values(3, buttonRead3, button3);
+  button4 = print_button_values(4, buttonRead4, button4);
 
   delay(100);
 
+}
+
+int print_pot_values(byte pot_num, int last, int current)
+{
+  // Only print values if significantly different from previously recorded value
+  if (abs(last - current) > 10)
+  {
+    Serial.print("Pot");Serial.print(pot_num);Serial.print(" = "); 
+    Serial.println(current); 
+
+    return current;
+  }
+  else 
+  {
+    return last;
+  }
+}
+
+byte print_button_values(byte but_num, int current_read, byte counter)
+{
+  if (current_read == LOW) {
+     counter = counter + 1;
+     Serial.print("Button");Serial.print(but_num);Serial.print(" = "); 
+     Serial.println(counter); 
+  }
+
+  return counter;
 }
